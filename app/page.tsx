faa1b09115d0a8272edc5a3c0ae72c6c6adca179
Dany Mrushi <mrushidany@gmail.com>
2024-01-16 18:38:57 +0300

    Customized the authwrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 import AuthWrapper from "./_components/auth/wrapper"
-import { usePathname } from "next/navigation"
 
 export default function Login() {
-  const pathname = usePathname()
-
   return (
     <>
-      <AuthWrapper path={pathname ?? pathname}></AuthWrapper>
+      <AuthWrapper />
     </>
   )
 }
