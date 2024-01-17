626ab5e41bdf3fbfcbebba5b1cd56bfdcdea4a85
Dany Mrushi <mrushidany@gmail.com>
2024-01-17 16:43:16 +0300

    Tab functionality troubleshooting

--- app/dashboard/user-management/page.tsx
+++ app/dashboard/user-management/page.tsx
@@ -15,16 +15,6 @@ export default function UserManagement() {
     teachers:false,
   })
 
-  const handleActiveTab = (activeTab: string): void => {
-    setTab((prev) => ({
-      admins: false,
-      students: false,
-      parents: false,
-      teachers: false,
-      [activeTab]: true,
-    }));
-  };
-
   const tabList = [
     {
       name: 'Admins',
@@ -44,18 +34,29 @@ export default function UserManagement() {
     },
   ]
 
+  const handleActiveTab = (activeTab: keyof Tab): void => {
+    setTab((prev) => ({
+      admins: false,
+      students: false,
+      parents: false,
+      teachers: false,
+      [activeTab]: true,
+    }));
+  };
+
   return (
     <>
       <div className='w-full h-full p-6'>
         <div className='flex flex-col gap-5'>
-          <div className='flex flex-row justify-between'>
+          <div className='flex flex-row justify-between text-[20px]'>
             <div className='flex flex-row gap-[10px]'>
               {tabList?.map((item, index) => (
-                <button key={index} className={`w-[128px] h-[60px] rounded-[5px] text-[20px] font-medium flex items-center justify-center ${tab[item.tab as keyof Tab] ? 'bg-orange-default text-white-default' : 'text-orange-default border-[0.5px] border-solid border-orange-200 bg-orange-400'}`} onClick={() => handleActiveTab(item.tab as keyof Tab)}>
+                <button key={index} className={`w-[128px] h-[60px] rounded-[5px] flex items-center justify-center ${tab[item.tab as keyof Tab] ? 'bg-orange-default text-white-default' : 'text-orange-default border-[0.5px] border-solid border-orange-200 bg-orange-400'}`} onClick={() => handleActiveTab(item.tab as keyof Tab)}>
                   {item.name}
                 </button>
               ))}
             </div>
+            <button className='w-[178px] h-[60px] rounded-[5px] bg-orange-default text-white-default flex items-center justify-center'>Add {tab.admins ? 'Admin' : (tab.parents ? 'Parent' : (tab.students ? 'Student' : 'Teacher'))} +</button>
           </div>
         </div>
       </div>
