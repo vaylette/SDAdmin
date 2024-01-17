189f1c604482fa1e53d5298a8b72bed90ab021ec
Dany Mrushi <mrushidany@gmail.com>
2024-01-17 11:49:22 +0300

    Changed the margin dimensions in the overview card section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default function Dashboard() {
   return (
     <>
-      <div className='w-full h-full py-8 px-6'>
+      <div className='w-full h-full p-8'>
         <div className='grid grid-cols-4 gap-5'>
           <div className='w-[260px] h-[200px] bg-overview bg-white-default rounded-[10px] '>
             
