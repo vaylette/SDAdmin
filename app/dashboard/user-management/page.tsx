7e1d833e43c4f87b5d837169b364ee8e16cc5370
Dany Mrushi <mrushidany@gmail.com>
2024-01-17 15:31:17 +0300

    Customizing the user management workspace

--- app/dashboard/user-management/page.tsx
+++ app/dashboard/user-management/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+import { useState } from "react"
 
+interface Tab {
+  admins: boolean,
+  students: boolean,
+  parents: boolean,
+  teachers: boolean
+}
 export default function UserManagement() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='w-full h-full p-6'>
+        <div className='flex flex-col gap-5'>
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-row gap-[10px]'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
