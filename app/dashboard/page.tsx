e602568aacc499ac1990b06bd941608e0fce305c
Dany Mrushi <mrushidany@gmail.com>
2024-01-17 11:33:31 +0300

    Added the overview card background image

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
 
 export default function Dashboard() {
   return (
-    <div>
-        
-    </div>
+    <>
+      <div className='w-full h-full p-8'>
+        <div className='grid grid-cols-4 gap-5'>
+          <div className='w-[263px] h-[200px] bg-white-default rounded-[10px]'>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
