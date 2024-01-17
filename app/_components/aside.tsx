e9ccbbe6ec237f0386f090fa769846268b30c62f
Dany Mrushi <mrushidany@gmail.com>
2024-01-17 10:15:28 +0300

    Updated the sidebar navlinks and its hover and active properties

--- app/_components/aside.tsx
+++ app/_components/aside.tsx
@@ -1,15 +1,19 @@
+'use client'
 import Image from 'next/image'
 import logo from '../../public/images/vertical_logo.png'
+import { usePathname } from 'next/navigation'
 
 export default function Aside() {
+    const pathname = usePathname()
+    console.log(pathname)
   return (
     <>
       <aside className="h-full lg:w-[454px] 2xl:w-[404px] bg-orange-default flex flex-col items-center">
          <div className="pt-[60px]">
             <Image src={logo} className="w-[159px] h-[91.2px]" alt="SmartDarasa" />
         </div>
-        <nav className="flex flex-col pt-[82px] text-lg text-white-default">
-            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center cursor-pointer gap-[22px] radial`}>
+        <nav className="flex flex-col pt-[82px] gap-3 text-lg text-white-default">
+            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center cursor-pointer gap-[22px] rounded-lg radial ${pathname === '/dashboard' && 'radial_active mb-2'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" viewBox="0 0 21 22" fill="none">
                     <path d="M20 14.9546C20.5523 14.9546 21 15.4023 21 15.9546L21 20.3823C21 20.9346 20.5523 21.3823 20 21.3823L1.10989 21.3823C0.557602 21.3823 0.109886 20.9346 0.109886 20.3823L0.109886 15.9546C0.109886 15.4023 0.557602 14.9546 1.10989 14.9546L20 14.9546Z" fill="white"/>
                     <path d="M11.1619 0.492187C11.7141 0.492187 12.1619 0.939903 12.1619 1.49219L12.1619 11.5442C12.1619 12.0965 11.7141 12.5442 11.1619 12.5442L0.999979 12.5442C0.447695 12.5442 -1.95807e-05 12.0965 -1.95566e-05 11.5442L-2.00709e-05 1.49219C-2.00467e-05 0.939901 0.447695 0.492187 0.99998 0.492187L11.1619 0.492187Z" fill="white"/>
@@ -18,13 +22,13 @@ export default function Aside() {
                 </svg>
                 <span>Overview</span>
             </li>
-            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center gap-[22px] cursor-pointer radial`}>
+            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center gap-[22px] cursor-pointer radial rounded-lg ${pathname === '/user-management' && 'radial_active mb-2'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="23" viewBox="0 0 25 23" fill="none">
                     <path fillRule="evenodd" clipRule="evenodd" d="M14.2856 5.35711C14.2856 8.31576 11.8872 10.7142 8.92852 10.7142C5.96987 10.7142 3.57141 8.31576 3.57141 5.35711C3.57141 2.39846 5.96987 0 8.92852 0C11.8872 0 14.2856 2.39846 14.2856 5.35711ZM8.92852 12.4999C3.99743 12.4999 0 16.4973 0 21.4284C0 21.9215 0.399743 22.3212 0.892852 22.3212H16.9642C17.4573 22.3212 17.857 21.9215 17.857 21.4284C17.857 16.4973 13.8596 12.4999 8.92852 12.4999ZM24.1071 22.3212H19.9599C20.0441 22.0383 20.0893 21.7386 20.0893 21.4284C20.0893 17.7836 18.3422 14.547 15.6398 12.5101C15.7828 12.5033 15.9267 12.4999 16.0715 12.4999C21.0026 12.4999 25 16.4973 25 21.4284C25 21.9215 24.6003 22.3212 24.1071 22.3212ZM16.0715 10.7142C15.5327 10.7142 15.0125 10.6347 14.5219 10.4867C15.7614 9.13593 16.5179 7.33487 16.5179 5.35711C16.5179 3.37935 15.7614 1.57829 14.5219 0.227523C15.0125 0.0795418 15.5327 0 16.0715 0C19.0301 0 21.4286 2.39846 21.4286 5.35711C21.4286 8.31576 19.0301 10.7142 16.0715 10.7142Z" fill="white"/>
                 </svg>
                 <span>User Management</span>
             </li>
-            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center gap-[22px] cursor-pointer radial`}>
+            <li className={`w-auto h-auto py-[22px] px-8 flex flex-row items-center gap-[22px] cursor-pointer radial rounded-lg ${pathname === '/content-management' && 'radial_active mb-2'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
                     <path d="M14.4465 0.0163865C13.7157 -0.0985971 13.0306 0.403804 12.9163 1.13853C12.8019 1.87326 13.3016 2.56208 14.0324 2.67707C18.7284 3.41597 22.3214 7.50355 22.3214 12.4324C22.3214 17.8859 17.9242 22.3069 12.5 22.3069C8.35466 22.3069 4.80601 19.7244 3.36502 16.0674C3.09259 15.3761 2.31427 15.0376 1.6266 15.3115C0.938933 15.5854 0.602317 16.368 0.874749 17.0593C2.70661 21.7083 7.21932 25 12.5 25C19.4035 25 25 19.3733 25 12.4324C25 6.15648 20.4259 0.957215 14.4465 0.0163865Z" fill="white"/>
                     <path d="M11.0068 1.05229C11.1912 1.77249 10.76 2.5066 10.0436 2.69196C9.59255 2.80869 9.15401 2.95687 8.73042 3.13405C8.04749 3.41971 7.26355 3.09467 6.97943 2.40805C6.6953 1.72144 7.01859 0.933251 7.70152 0.647591C8.24154 0.421707 8.80075 0.232752 9.37598 0.0838969C10.0923 -0.101469 10.8225 0.332097 11.0068 1.05229Z" fill="white"/>
