f59159ec8fbf092f677e23141b2b097d99a4641d
Dany Mrushi <mrushidany@gmail.com>
2024-01-18 19:49:52 +0300

    Created the add admin form

--- app/dashboard/user-management/page.tsx
+++ app/dashboard/user-management/page.tsx
@@ -292,7 +292,7 @@ export default function UserManagement() {
           </div>
         </div>
       </div>
-      <div className={`${modal.create || modal.edit ? 'fixed inset-0 bg-black-100 bg-blend-multiply z-50 justify-end' : 'hidden'}`}>
+      <div className={`${modal.create || modal.edit ? 'absolute min-h-screen inset-0 bg-black-100 bg-blend-multiply z-50 justify-end' : 'hidden'}`}>
         <div className='w-full h-full flex justify-end'>
           <div className='block bg-white-100 w-[432px] h-full relative pt-[100px] px-[50px]'>
             <div className='absolute top-10 -left-[50px]'>
@@ -306,6 +306,33 @@ export default function UserManagement() {
             <div className='flex flex-col gap-10'>
                 <h6 className='text-black-100 text-3xl font-bold'>Add Admin</h6>
                 <div className='w-full h-[1px] bg-black-200'></div>
+                <form className='flex flex-col gap-5 text-lg text-black-400 pb-[92px]'>
+                  <div className='flex flex-col gap-2'>
+                    <label className=''>First name</label>
+                    <input type='text' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <label className=''>Last name</label>
+                    <input type='text' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <label className=''>Email</label>
+                    <input type='email' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <label className=''>Add phone number</label>
+                    <input type='phone' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <label className=''>Permission level</label>
+                    <select className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0'>
+                      <option selected disabled>Select permission</option>
+                      <option>Level 1 (Content upload)</option>
+                      <option>Level 2 (Content download)</option>
+                    </select>
+                  </div>
+                  <button className='w-full h-[60px] rounded-[30px] bg-orange-default flex items-center justify-center mt-[89px] text-white-default font-medium text-xl'>Confirm</button>
+                </form>
             </div>
           </div>
         </div>
