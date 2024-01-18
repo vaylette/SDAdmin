188395f258c301fc72edf0eb425e760d6aaba2dd
Dany Mrushi <mrushidany@gmail.com>
2024-01-18 19:18:28 +0300

    Removed the add button for other users other than the admin

--- app/dashboard/user-management/page.tsx
+++ app/dashboard/user-management/page.tsx
@@ -228,7 +228,9 @@ export default function UserManagement() {
                 </button>
               ))}
             </div>
-            <button onClick={() => handleModal('create')} className='w-[178px] h-[60px] rounded-[5px] bg-orange-default text-white-default flex items-center justify-center'>Add {tab.admins ? 'Admin' : (tab.parents ? 'Parent' : (tab.students ? 'Student' : 'Teacher'))} +</button>
+            {tab.admins && (
+              <button onClick={() => handleModal('create')} className='w-[178px] h-[60px] rounded-[5px] bg-orange-default text-white-default flex items-center justify-center'>Add Admin +</button>
+            )}
           </div>
           <div className='flex flex-col gap-10 text-[15px]'>
             <table className='min-w-full h-auto rounded-[10px] text-black-100 bg-white-default'>
@@ -292,7 +294,7 @@ export default function UserManagement() {
       </div>
       <div className={`${modal.create || modal.edit ? 'fixed inset-0 bg-black-100 bg-blend-multiply z-50 justify-end' : 'hidden'}`}>
         <div className='w-full h-full flex justify-end'>
-          <div className='block bg-white-100 w-[432px] h-full relative'>
+          <div className='block bg-white-100 w-[432px] h-full relative pt-[100px] px-[50px]'>
             <div className='absolute top-10 -left-[50px]'>
               <button onClick={handleModalClose} className='w-[50px] h-[60px] bg-orange-default flex items-center justify-center rounded-l-sm'>
                 <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -300,6 +302,10 @@ export default function UserManagement() {
                   <path d="M1 1L16 16" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
               </button>
+            </div>
+            <div className='flex flex-col gap-10'>
+                <h6 className='text-black-100 text-3xl font-bold'>Add {tab.admins ? 'Admin' : (tab.parents ? 'Parent' : (tab.students ? 'Student' : 'Teacher'))}</h6>
+                <div className='w-full h-[1px] bg-black-200'></div>
             </div>
           </div>
         </div>
