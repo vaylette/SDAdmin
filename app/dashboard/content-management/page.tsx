0e64b321779cecb2a42043ba34be093ae4930f3d
Dany Mrushi <mrushidany@gmail.com>
2024-01-19 14:23:58 +0300

    Defining the columns

--- app/dashboard/content-management/page.tsx
+++ app/dashboard/content-management/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import OverviewCard from "@/app/_components/overviewCard"
 import { TabComponent, TabItem } from "@/app/_components/tab"
+import DataTable from "@/app/_components/datatable";
 
 interface ContentTab {
   topics: boolean;
@@ -9,6 +10,18 @@ interface ContentTab {
   experiments: boolean;
   videos: boolean;
   [key: string]: boolean;
+}
+
+type User = {
+  ref_no: string | number | null | undefined
+  topic_name: string,
+  subject: string,
+  level: string,
+  curriculum: string,
+  sections: string | number,
+  questions: string | number,
+  uploaded: string | number,
+  action: null
 }
 
 export default function ContentManagement() {
@@ -19,7 +32,7 @@ export default function ContentManagement() {
     videos:false,
   })
 
-  const data = [
+  const overviewData = [
     {
       title: 'Total Topics',
       quantity: 173,
@@ -59,26 +72,31 @@ export default function ContentManagement() {
     }));
   };
 
+  
+
   return (
     <>
       <div className='flex flex-col'>
         <div className='grid grid-cols-4 2xl:flex 2xl:flex-row 2xl:flex-wrap gap-5'>
-          {data.map((item, index) => (
+          {overviewData.map((item, index) => (
             <OverviewCard key={index} title={item.title} quantity={item.quantity} growth={item.growth} />
           ))}
         </div>
         <div className='mt-9 w-full flex flex-row justify-between items-center'>
-            <div className="flex flex-row items-center gap-9">
-              <TabComponent tab={tab} tabList={tabList} handleActiveTab={handleActiveTab} />
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
-                  <path d="M15 15L22 22M9.16667 17.3333C4.65634 17.3333 1 13.677 1 9.16667C1 4.65634 4.65634 1 9.16667 1C13.677 1 17.3333 4.65634 17.3333 9.16667C17.3333 13.677 13.677 17.3333 9.16667 17.3333Z" stroke="#FF9D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-              </button>
-            </div>
-            {tab.topics && (
-              <button className='w-[178px] h-[60px] rounded-[5px] bg-orange-default text-white-default flex items-center justify-center'>Add Topic +</button>
-            )}
+          <div className="flex flex-row items-center gap-9">
+            <TabComponent tab={tab} tabList={tabList} handleActiveTab={handleActiveTab} />
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                <path d="M15 15L22 22M9.16667 17.3333C4.65634 17.3333 1 13.677 1 9.16667C1 4.65634 4.65634 1 9.16667 1C13.677 1 17.3333 4.65634 17.3333 9.16667C17.3333 13.677 13.677 17.3333 9.16667 17.3333Z" stroke="#FF9D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
+          </div>
+          {tab.topics && (
+            <button className='w-[178px] h-[60px] rounded-[5px] bg-orange-default text-white-default flex items-center justify-center'>Add Topic +</button>
+          )}
+        </div>
+        <div className='mt-5'>
+            <DataTable />
         </div>
       </div>
     </>
