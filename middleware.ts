292c7ddd1a661e586a4abe2ef19c4c45108542e8
Dany Mrushi <mrushidany@gmail.com>
2024-01-29 12:48:54 +0300

    Created the middleware

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+'use client'
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl
+
+    if (
+        request.nextUrl.pathname.startsWith('/api/') ||
+        request.nextUrl.pathname.startsWith('/_next/')
+    ) {
+        return NextResponse.next()
+    }
+
+    let authenticated = request.cookies.get("token")?.value
+    const allowedRoutes = ['/',]
+    const isRouteAllowed = allowedRoutes.includes(pathname) 
+
+    if (!authenticated) {
+        if (isRouteAllowed) {
+          return NextResponse.next()
+        }
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+    
+    if (isRouteAllowed && authenticated) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+}
+
+export const config = {
+    matcher: ['/','/dashboard']
+}
