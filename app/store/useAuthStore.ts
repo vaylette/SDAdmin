41f722637cfce99eb39dcd0f40642feca5099cdd
Dany Mrushi <mrushidany@gmail.com>
2024-01-29 16:07:17 +0300

    Set up the auth store

---
+++ app/store/useAuthStore.ts
@@ -0,0 +1,40 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { SmartUser } from '../types/User'
+
+export type AuthStore = {
+  authenticated: boolean;
+  user: SmartUser | any;
+  token: string | null;
+  setAuthentication: (authenticated: boolean) => void;
+  setUser: (user: SmartUser | null) => void;
+  setToken: (token: string) => void;
+  logout: () => void;
+}
+
+const useAuthStore = create(
+    persist(
+        (set, get): AuthStore => ({
+            authenticated: false,
+            user: null,
+            token: null,
+            setAuthentication: (val) => set({ authenticated: val }),
+            setUser: (val) => set({ user: val }),
+            setToken: (val) => set({ token: val}),
+            logout: () => {
+                set({
+                    authenticated: false,
+                    user: null,
+                    token: null,
+                })
+
+                document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:00 UTC; secure;'
+            }
+        }),
+        {
+            name: 'auth'
+        }
+    )
+)
+
+export default useAuthStore
