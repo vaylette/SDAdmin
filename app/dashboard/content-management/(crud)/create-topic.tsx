967d4cd68f789e87412457e6ae921715501780df
Dany Mrushi <mrushidany@gmail.com>
2024-01-30 14:49:42 +0300

    Installed the hero react icons

--- app/dashboard/content-management/(crud)/create-topic.tsx
+++ app/dashboard/content-management/(crud)/create-topic.tsx
@@ -1,3 +1,7 @@
+'use client'
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import ChevronUpd
 
 export default function CreateTopic({ subjects } : { subjects: {} }) {
     console.log(subjects)
@@ -24,7 +28,7 @@ export default function CreateTopic({ subjects } : { subjects: {} }) {
             <label className=''>Subjects</label>
             <select className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0'>
               <option selected disabled>Select subject</option>
-              
+
               <option>NECTA</option>
               <option>CAMBRIDGE</option>
             </select>
