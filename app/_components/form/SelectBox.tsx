78e584973c88bab36885143c0e0df51f5af39d8c
Dany Mrushi <mrushidany@gmail.com>
2024-01-30 15:28:51 +0300

    Customize the styling of the select box

--- app/_components/form/SelectBox.tsx
+++ app/_components/form/SelectBox.tsx
@@ -18,11 +18,11 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options }) => {
         <div className="w-full">
             <Listbox value={selected} onChange={handleChange}>
                 <div className="relative mt-1">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <Listbox.Button className="relative w-full cursor-default rounded-[4px] bg-black-500 h-[60px] text-start py-2 pl-3 pr-10 text-black-400 shadow-md focus:outline-none focus:ring-0">
                     <span className="block truncate">{selected.name}</span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <ChevronUpDownIcon
-                        className="h-5 w-5 text-gray-400"
+                        className="h-5 w-5 text-black-400"
                         aria-hidden="true"
                     />
                     </span>
@@ -33,13 +33,13 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options }) => {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                 >
-                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white-100 py-1 shadow-lg ring-1 ring-black-200 focus:outline-none text-sm">
                     {options.map((item, index) => (
                         <Listbox.Option
                         key={index}
                         className={({ active }) =>
                             `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                            active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                            active ? 'bg-orange-default text-white-default' : 'text-black-400'
                             }`
                         }
                         value={item}
@@ -54,7 +54,7 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options }) => {
                                 {item.name}
                             </span>
                             {selected ? (
-                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-100">
                                 <CheckIcon className="h-5 w-5" aria-hidden="true" />
                                 </span>
                             ) : null}
