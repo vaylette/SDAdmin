c98956d314497bf73a0a4cb3d578816f61e88ab6
Dany Mrushi <mrushidany@gmail.com>
2024-01-30 14:41:51 +0300

    Installed the headless ui

--- app/dashboard/content-management/(crud)/create-topic.tsx
+++ app/dashboard/content-management/(crud)/create-topic.tsx
@@ -21,14 +21,14 @@ export default function CreateTopic({ subjects } : { subjects: {} }) {
             <textarea rows={4} className='w-full bg-black-500 rounded-[4px] h-auto text-black-400 p-2 focus:outline-none focus:ring-0'  />
           </div>
           <div className='flex flex-col gap-2'>
-            <label className=''>Email</label>
-            <input type='email' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
+            <label className=''>Subjects</label>
+            <select className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0'>
+              <option selected disabled>Select subject</option>
+              
+              <option>NECTA</option>
+              <option>CAMBRIDGE</option>
+            </select>
           </div>
-          <div className='flex flex-col gap-2'>
-            <label className=''>Add phone number</label>
-            <input type='phone' className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0' />
-          </div>
-          
           <button className='w-full h-[60px] rounded-[30px] bg-orange-default flex items-center justify-center mt-[89px] text-white-default text-xl'>Save Changes</button>
         </form>
     </>
