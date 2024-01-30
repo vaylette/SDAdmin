a15a1b23b063242dbc03a582f1a56c298b664e7a
Dany Mrushi <mrushidany@gmail.com>
2024-01-30 14:22:36 +0300

    Rearrangement of the api urls

--- app/constants/apiUrls.ts
+++ app/constants/apiUrls.ts
@@ -1,10 +1,13 @@
 export const baseUrl = 'http://34.207.195.210/api/v1'
 
 export const apiUrls = {
+    // Authentication
     logIn: `${baseUrl}/auth/login`,
-    topics: `${baseUrl}/topics`,
-    models: `${baseUrl}/models/all`,
-    experiments: `${baseUrl}/experiments/all`,
-    videos: `${baseUrl}/videos/all`,
+
+    // Content Management
+    getTopics: `${baseUrl}/topics`,
+    getModels: `${baseUrl}/models/all`,
+    getExperiments: `${baseUrl}/experiments/all`,
+    getVideos: `${baseUrl}/videos/all`,
 
 }
