245318a946655274a0cdc40544ac67304bb74062
Dany Mrushi <mrushidany@gmail.com>
2024-01-30 15:15:20 +0300

    Created and integrated the select box

--- app/dashboard/content-management/(crud)/create-topic.tsx
+++ app/dashboard/content-management/(crud)/create-topic.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
-import ChevronUpd
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/16/solid'
+import SelectBox from '@/app/_components/form/SelectBox';
 
-export default function CreateTopic({ subjects } : { subjects: {} }) {
-    console.log(subjects)
+export interface Subject {
+    _id: string;
+    isActive: boolean;
+    isDeleted: boolean;
+    name: string;
+    __v: number;
+    createdAt: string;
+    updatedAt: string;
+}
+  
+interface CreateTopicProps {
+  subjects: Subject[];
+}
+  
+
+export default function CreateTopic({ subjects } : CreateTopicProps) {
+  const options = subjects?.map(subject => ({ name: subject.name}))
   return (
     <>
         <form className='flex flex-col gap-5 text-lg text-black-400 pb-[92px]'>
@@ -26,12 +42,7 @@ export default function CreateTopic({ subjects } : { subjects: {} }) {
           </div>
           <div className='flex flex-col gap-2'>
             <label className=''>Subjects</label>
-            <select className='w-full bg-black-500 rounded-[4px] h-[60px] text-black-400 px-2 focus:outline-none focus:ring-0'>
-              <option selected disabled>Select subject</option>
-
-              <option>NECTA</option>
-              <option>CAMBRIDGE</option>
-            </select>
+            <SelectBox options={options} />
           </div>
           <button className='w-full h-[60px] rounded-[30px] bg-orange-default flex items-center justify-center mt-[89px] text-white-default text-xl'>Save Changes</button>
         </form>
