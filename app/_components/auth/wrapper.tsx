fa2387c2eb0303ff32579a7c977e08792ce5dde5
Emmanuel Mtera <peterkelvin16@gmail.com>
2024-03-18 14:44:57 +0300

    admin fixes

--- app/_components/auth/wrapper.tsx
+++ app/_components/auth/wrapper.tsx
@@ -28,12 +28,12 @@ export default function AuthWrapper() {
     const [loading, setLoading] = useState(false)
 
     const handleChange = (event: any) => {
-        const {name, value} = event.target
+        const { name, value } = event.target
         setForm(prevForm => {
-          return {
-            ...prevForm,
-            [name]: value,
-          }
+            return {
+                ...prevForm,
+                [name]: value,
+            }
         })
     }
 
@@ -43,22 +43,26 @@ export default function AuthWrapper() {
 
         try {
             const response = await authenticate(form)
-            if(response.status === 401){
+            if (response.status === 401) {
                 toast.error(response.data.message)
             } else {
-                setAuthentication(true)
-                setUser(response.user)
-                setToken(response.access_token)
-                cookies.set('token', `${response.access_token}`, { secure: false })
-                toast.success('Successfully authenticated!')
-                router.push('/dashboard')
+                if (response.user.type === "Admin") {
+                    setAuthentication(true)
+                    setUser(response.user)
+                    setToken(response.access_token)
+                    cookies.set('token', `${response.access_token}`, { secure: false })
+                    toast.success('Successfully authenticated!')
+                    router.push('/dashboard')
+                } else {
+                    toast.error('Unauthorized to access admin')
+                }
             }
         } catch (error) {
             toast.error('Authentication failed')
-        } finally { 
+        } finally {
             setLoading(false)
         }
-        
+
     }
 
     return (
@@ -73,38 +77,38 @@ export default function AuthWrapper() {
                 <div className='w-full h-[1px] bg-black-300'></div>
             </div>
             <div className='my-[97px] flex justify-center'>
-                    <div className='w-auto h-auto rounded-[20px] drop-shadow-auth'>
-                        <div className='flex flex-row h-[547px]'>
-                            <div>
-                                <Image className='w-[350px] h-full' priority src={bulb} alt='SmartDarasa' />
-                            </div>
-                            <div className='w-auto h-full flex items-center bg-white-300 py-[50px] px-[55px] rounded-r-[20px]'>
-                                <div className='flex flex-col gap-[26px]'>
-                                    <div className='flex flex-col gap-[10px]'>
-                                        <h5 className='text-orange-default text-[40px] font-bold leading-[30px]'>Welcome back!</h5>
-                                        <p className='text-black-300 text-[20px] leading-[30px]'>Enter your details to continue</p>
-                                    </div>
-                                    <form onSubmit={handleSubmit} className='flex flex-col items-center gap-[10px] w-[377px]'>
-                                        <input type='email' onChange={handleChange} value={form.username} name="username" className='w-full h-[54px] rounded-[5px] bg-black-200 px-6 py-5 text-black-300 text-lg leading-[21px] placeholder:text-black-300 placeholder:text-lg placeholder:leading-[21px] focus:ring-0 focus:outline-none' placeholder='Email address' />
-                                        <input type='password' onChange={handleChange} value={form.password} name="password" className='w-full h-[54px] rounded-[5px] bg-black-200 px-6 py-5 text-black-300 text-lg leading-[21px] placeholder:text-black-300 placeholder:text-lg placeholder:leading-[21px] focus:ring-0 focus:outline-none' placeholder='Password' />
-                                        <div className='mt-[10px]'>
-                                            <button type='submit' className={`w-[377px] h-[54px] bg-orange-default uppercase text-white-default text-lg font-medium leading-[21px] flex items-center justify-center rounded-full ${loading ? 'flex flex-row gap-2 items-center' : ''}`} disabled={loading}>
-                                                <span>log in</span>
-                                                {loading && (
-                                                    <svg height="40" width="40" className="text-white-default">
-                                                        <circle className="dot" cx="10" cy="20" r="3" />
-                                                        <circle className="dot" cx="20" cy="20" r="3" />
-                                                        <circle className="dot" cx="30" cy="20" r="3" />
-                                                  </svg>
-                                                )}
-                                            </button>
-                                        </div>
-                                    </form>
+                <div className='w-auto h-auto rounded-[20px] drop-shadow-auth'>
+                    <div className='flex flex-row h-[547px]'>
+                        <div>
+                            <Image className='w-[350px] h-full' priority src={bulb} alt='SmartDarasa' />
+                        </div>
+                        <div className='w-auto h-full flex items-center bg-white-300 py-[50px] px-[55px] rounded-r-[20px]'>
+                            <div className='flex flex-col gap-[26px]'>
+                                <div className='flex flex-col gap-[10px]'>
+                                    <h5 className='text-orange-default text-[40px] font-bold leading-[30px]'>Welcome back!</h5>
+                                    <p className='text-black-300 text-[20px] leading-[30px]'>Enter your details to continue</p>
                                 </div>
+                                <form onSubmit={handleSubmit} className='flex flex-col items-center gap-[10px] w-[377px]'>
+                                    <input type='email' onChange={handleChange} value={form.username} name="username" className='w-full h-[54px] rounded-[5px] bg-black-200 px-6 py-5 text-black-300 text-lg leading-[21px] placeholder:text-black-300 placeholder:text-lg placeholder:leading-[21px] focus:ring-0 focus:outline-none' placeholder='Email address' />
+                                    <input type='password' onChange={handleChange} value={form.password} name="password" className='w-full h-[54px] rounded-[5px] bg-black-200 px-6 py-5 text-black-300 text-lg leading-[21px] placeholder:text-black-300 placeholder:text-lg placeholder:leading-[21px] focus:ring-0 focus:outline-none' placeholder='Password' />
+                                    <div className='mt-[10px]'>
+                                        <button type='submit' className={`w-[377px] h-[54px] bg-orange-default uppercase text-white-default text-lg font-medium leading-[21px] flex items-center justify-center rounded-full ${loading ? 'flex flex-row gap-2 items-center' : ''}`} disabled={loading}>
+                                            <span>log in</span>
+                                            {loading && (
+                                                <svg height="40" width="40" className="text-white-default">
+                                                    <circle className="dot" cx="10" cy="20" r="3" />
+                                                    <circle className="dot" cx="20" cy="20" r="3" />
+                                                    <circle className="dot" cx="30" cy="20" r="3" />
+                                                </svg>
+                                            )}
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
